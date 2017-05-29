e3912578e9a1d35648dcd4dcd932e43fa65e1bce
Frank Sachsenheim <funkyfuture@riseup.net>
2017-05-29 16:26:04 +0200

    Fixes missing typing import
    also a reorders sections in tox.ini

--- deck_chores/indexes.py
+++ deck_chores/indexes.py
@@ -1,2 +1,4 @@
+from typing import Dict  # noqa: F401
+
 locking_container_to_services_map = {}  # type: Dict[str, str]
 """ A mapping of locking container ids to service ids whose jobs have been added. """
