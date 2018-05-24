189bc996a89e9c2680588b417a0fee5c86a956a6
Frank Sachsenheim <funkyfuture@riseup.net>
2018-05-24 20:10:55 +0200

    Updates used Cerberus version to 1.2

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     package_dir={'deck_chores': 'deck_chores'},
     include_package_data=True,
     install_requires=[
-        'APScheduler~=3.5', 'cerberus~=1.1', 'docker~=2.7', 'fasteners~=0.14'
+        'APScheduler~=3.5', 'cerberus~=1.2', 'docker~=2.7', 'fasteners~=0.14'
     ],
     license="ISC license",
     zip_safe=False,
