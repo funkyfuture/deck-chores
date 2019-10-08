fdce06d49dedd658121d87e023a1a036c0c311f1
Frank Sachsenheim <funkyfuture@riseup.net>
2019-10-08 13:08:02 +0200

    Bumps version to 1.0-dev

--- setup.py
+++ setup.py
@@ -1,27 +1,17 @@
-#!/usr/bin/env python
-
-from os import chdir, getcwd
 from pathlib import Path
 from setuptools import setup
 
 
-VERSION = '0.3.1'
-
-_old_cwd = getcwd()
-chdir(Path(__file__).parent)
-
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
+PROJECT_FOLDER = Path(__file__).parent.resolve()
+VERSION = "1.0-dev"
 
 setup(
     name='deck-chores',
     version=VERSION,
     description="Job scheduler for Docker containers, configured via container labels.",
-    long_description=readme + '\n\n' + history,
+    long_description=(PROJECT_FOLDER / "README.rst").read_text()
+    + '\n\n'
+    + (PROJECT_FOLDER / "HISTORY.rst").read_text(),
     author="Frank Sachsenheim",
     author_email='funkyfuture@riseup.net',
     url='https://github.com/funkyfuture/deck-chores',
@@ -47,5 +37,3 @@ setup(
     ],
     entry_points={'console_scripts': ['deck-chores = deck_chores.main:main']},
 )
-
-chdir(_old_cwd)
